bfbd2f716aa4c4c7a2aacd4a76f071eb1a1089a3
SCSBoy <143944044+SCSBoy@users.noreply.github.com>
2024-06-19 12:03:10 +0000

    Options.dart

--- lib/Options.dart
+++ lib/Options.dart
@@ -1,49 +1,61 @@
 import 'package:flutter/material.dart';
 
-// ignore: must_be_immutable
-class Options extends StatelessWidget {
-  String option;
+class Options extends StatefulWidget {
+  final String option;
+
   Options({Key? key, required this.option}) : super(key: key);
+
+  @override
+  _OptionsState createState() => _OptionsState();
+}
+
+class _OptionsState extends State<Options> {
+  String? _selectedValue;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          height: 48,
-          width: 240,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            border: Border.all(
-              width: 3,
-              color: Color(0xffA42FC1),
-            ),
-          ),
-          child: Center(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 18),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    option,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Radio(
-                    value: option,
-                    groupValue: 2,
-                    onChanged: (val) {},
-                  ),
-                ],
+        _buildOptionWidget(),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+
+  Widget _buildOptionWidget() {
+    return Container(
+      height: 48,
+      width: 240,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          width: 3,
+          color: Color(0xffA42FC1),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 18),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.option,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
+            Radio(
+              value: widget.option,
+              groupValue: _selectedValue,
+              onChanged: (val) {
+                setState(() {
+                  _selectedValue = val as String?;
+                });
+              },
+            ),
+          ],
         ),
-        SizedBox(
-          height: 10,
-        ),
-      ],
+      ),
     );
   }
 }
